01fde7031fc6972d1befdb6a531145cd9e432a32
David de Jesús Martínez Mayeta <imdj9003@gmail.com>
2026-02-10 21:29:30 -0500

    refactor(index.tsx): remove unused render result variable

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const _root = ReactDOM.render(
+void ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,

e4c4f2c0cb6dda71040e232cb7f2c3ff2e6d4447
David de Jesús Martínez Mayeta <imdj9003@gmail.com>
2026-02-10 21:22:09 -0500

    refactor: rename root variable to _root

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.render(
+const _root = ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
